3a0b233dbf263f7bdec065c6ef347487763f2319
Mike Kelly <mike@kellyfam.net>
2015-09-15 20:28:00 +0000

    hook up awards and pending awards to child

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,7 +5,14 @@ class Child < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
 
   has_many :associated_steps, inverse_of: :child
+  has_many :awarded_associated_steps, -> { where.not(award_received_at: nil)}, class_name: "AssociatedStep", inverse_of: :child
+  has_many :pending_award_associated_steps, -> { where(award_received_at: nil)}, class_name: "AssociatedStep", inverse_of: :child
   has_many :completed_steps, through: :associated_steps, class_name: "Step", source: :step
+  has_many :awarded_steps, through: :awarded_associated_steps, class_name: "Step", source: :step
+  has_many :pending_award_steps, through: :pending_award_associated_steps, class_name: "Step", source: :step
+  
+  has_many :awards, through: :awarded_steps, source: :awards
+  has_many :pending_awards, class_name: "Award", through: :pending_award_steps, source: :awards
 
   def name
     "#{first_name} #{last_name}"
